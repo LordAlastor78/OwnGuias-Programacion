44ca8ce63d54216a80839763dcf6f80669490492
Lord_Alastor78 <192902520+LordAlastor78@users.noreply.github.com>
2026-02-04 17:30:08 +0100

    Add Ejercicio3 class to read and analyze integer characteristics

---
+++ doc/Prácticas/practica0/src/main/java/ejercicio3/Ejercicio3.java
@@ -0,0 +1,28 @@
+/*Ejercicio 3
+
+En un paquete ejercicio3, clase Ejercicio3, escribe un programa que lea por teclado un número entero e indique por pantalla, mediante mensajes que aparecerán en líneas diferentes, las características de dicho número:
+
+    "Número par" si el número es par y "Número impar" en caso contrario.
+    "Número cero" si el número es cero y "Número distinto de cero" en caso contrario.
+    "Número elevado" si el número es mayor de 100 y "Número bajo" en caso contrario.*/
+import java.util.Scanner;
+
+package ejercicio3;
+
+public class Ejercicio3 {
+
+
+    public static void main(String[] args) {
+
+        Scanner cin = new Scanner(System.in);
+
+        System.out.println("Introduce un número entero: ");
+
+        int numero = cin.nextInt();
+
+        
+
+
+        
+    }
+}
