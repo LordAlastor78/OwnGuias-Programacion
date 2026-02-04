8d31e27899126266f2b0dced62a0ad25e7e8ae7d
Lord_Alastor78 <192902520+LordAlastor78@users.noreply.github.com>
2026-02-04 17:44:17 +0100

    Ejercicio3

--- doc/Prácticas/practica0/src/main/java/ejercicio3/Ejercicio3.java
+++ doc/Prácticas/practica0/src/main/java/ejercicio3/Ejercicio3.java
@@ -1,3 +1,6 @@
+
+package ejercicio3;
+
 /*Ejercicio 3
 
 En un paquete ejercicio3, clase Ejercicio3, escribe un programa que lea por teclado un número entero e indique por pantalla, mediante mensajes que aparecerán en líneas diferentes, las características de dicho número:
@@ -7,22 +10,35 @@ En un paquete ejercicio3, clase Ejercicio3, escribe un programa que lea por tecl
     "Número elevado" si el número es mayor de 100 y "Número bajo" en caso contrario.*/
 import java.util.Scanner;
 
-package ejercicio3;
-
 public class Ejercicio3 {
-
 
     public static void main(String[] args) {
 
-        Scanner cin = new Scanner(System.in);
+        try (Scanner cin = new Scanner(System.in)) {
+            System.out.println("Introduce un número entero: ");
 
-        System.out.println("Introduce un número entero: ");
+            int numero = cin.nextInt();
 
-        int numero = cin.nextInt();
+            numero = cin.nextInt();
 
-        
+            if (numero % 2 == 0) {
+                System.out.println("Número par");
+            } else {
+                System.out.println("Número impar");
 
+                if (numero == 0) {
+                    System.out.println("Número cero");
+                } else {
+                    System.out.println("Número distinto de cero");
+                    if (numero > 100) {
+                        System.out.println("Número elevado");
+                    } else {
+                        System.out.println("Número bajo");
 
-        
+                    }
+
+                }
+            }
+        }
     }
 }
