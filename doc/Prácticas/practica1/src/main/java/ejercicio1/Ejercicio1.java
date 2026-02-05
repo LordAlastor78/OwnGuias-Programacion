7aabc540939149d52bfc5e4aa0b8bb0f7966e8af
Lord_Alastor78 <192902520+LordAlastor78@users.noreply.github.com>
2026-02-05 18:09:49 +0100

    working...

---
+++ doc/Prácticas/practica1/src/main/java/ejercicio1/Ejercicio1.java
@@ -0,0 +1,20 @@
+package ejercicio1;
+
+/*Ejercicio 1
+
+Los puntos son coordenadas cartesianas de
+dos dimensiones x e y que se expresan como "(x, y)".
+Es posible calcular la distancia al origen (que siempre es (0, 0)) 
+mediante la raíz cuadrada de la suma de las coordenadas 
+al cuadrado. Crea una clase para manejar dicha información. 
+*/
+
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+
+        
+        
+    }
+
+}
---
+++ doc/Prácticas/practica1/src/main/java/ejercicio1/Punto.java
@@ -0,0 +1,27 @@
+package ejercicio1;
+
+/*Ejercicio 1
+
+Los puntos son coordenadas cartesianas de
+dos dimensiones x e y que se expresan como "(x, y)".
+Es posible calcular la distancia al origen (que siempre es (0, 0)) 
+mediante la raíz cuadrada de la suma de las coordenadas 
+al cuadrado. Crea una clase para manejar dicha información. 
+*/
+
+public class Punto {
+
+    private double x;
+    private double y;
+
+    public Punto(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Object distanciaOrigen(int x, int y) {
+
+        
+    }
+
+}
