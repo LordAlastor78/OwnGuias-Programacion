fb7f70dbd3767ae79c95ee099d041188b471eb19
Lord_Alastor78 <192902520+LordAlastor78@users.noreply.github.com>
2026-02-10 17:28:57 +0100

    worked on class Ej1

--- doc/Prácticas/practica2/src/main/java/ejercicio1/Ejercicio1.java
+++ doc/Prácticas/practica2/src/main/java/ejercicio1/Ejercicio1.java
@@ -4,9 +4,34 @@ public class Ejercicio1 {
 
     public static void main(String[] args) {
         
-        
+    Libro libro1 =  new Libro("1984", "Orwell", "niIdea", 1984);
 
+/*     libro1.setAnho(-57); */
 
+    Libro [] biblioteca = new Libro[10];
+
+    int numLibros = 0;
+
+    biblioteca[0] = new Libro("El Quijote", "Cervantes", "niIdea", 1605);
+    numLibros++;
+    biblioteca[numLibros] = new Libro("El Señor de los Anillos", "Tolkien", "niIdea", 1954);
+    numLibros++;
+
+    if (numLibros == 0) {
+        System.out.println("No hay libros en la biblioteca");
+    } else {
+        for (int i = 0; i < numLibros; i++) {
+            System.out.println(biblioteca[i]);
+        }
+    }
+
+    System.out.println(libro1);
+
+    /*Mejora la encapsulación de la clase
+    Libro ocultando la representación interna,
+    aunque proveas de acceso a la 
+    información de forma adecuada y mantén 
+    la clase inmutable.------> use : final (declarar como constante) */
 
 
 
--- doc/Prácticas/practica2/src/main/java/ejercicio1/Libro.java
+++ doc/Prácticas/practica2/src/main/java/ejercicio1/Libro.java
@@ -7,21 +7,36 @@ class Libro {
     int anho;
     String isbn;
 
-    Libro(String titulo, String autores, String editorial, int anho, String isbn) {
+    // Constructor
+    Libro(String titulo, String autores, String editorial, int anho) {
         this.titulo = titulo;
         this.autores = autores;
         this.editorial = editorial;
         this.anho = anho;
-        this.isbn = isbn;
+        this.isbn = "desconocido";
     }
+
+    // Getters(devolver un atributo concreto) y Setters(modificar un atributo
+    // concreto)
+
+    public int getAnho() {
+        return anho;
+    }
+
+    // Removed setter with final to maintain immutability
+    /*
+    public void setAnho(int anho) {
+        if (anho > 0) this.anho = anho;
+
+    }*/
 
     @Override
     public String toString() {
         return """
-               Titulo = %s
-               Autores = %s
-               Editorial = %s
-               Año = %d
-               Isbn = %s""".formatted(titulo, autores, editorial, anho, isbn);
+                Titulo = %s
+                Autores = %s
+                Editorial = %s
+                Año = %d
+                Isbn = %s""".formatted(titulo, autores, editorial, anho, isbn);
     }
 }
